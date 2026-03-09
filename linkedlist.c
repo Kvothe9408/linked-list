935951b8cc6d8d8dac190d7f5373d107e27f342c
Kvothe <kvothe_94@proton.me>
2026-03-09 10:47:35 -0400

    create defintiions Node, linkedlist, overview of functions needed

---
+++ linkedlist.c
@@ -0,0 +1,23 @@
+// Standard Libaries
+#include <stdio.h> // standard input/outpu. printf, scanf, fgets
+#include <stdlib.h> // standard library. malloc, realloc, free, exit
+
+// My defitions
+typedef struct Node Node; // declares that "struct Node" as "Node" will be defined
+typedef struct Node{ // Defining "Node" as a struct
+    int data; //a pointer pointing to the data in this node
+    struct Node *next; //a pointer pointing to the next node head. using struct node as that is the general form and Node isn't defined yet at this stage
+} Node; // name of struct
+typedef struct{
+    Node *head; //a pointer pointing to the first node head in the linked list
+} linkedlist; // name of struct
+
+
+// functions
+create()
+push_front()
+push_back()
+delete_node()
+get()
+print()
+free_list()
